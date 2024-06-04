3aa087c64842089c9210d022d041e3594aa3f20f
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2024-06-04 11:35:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", None))
 OWNER_ID = int(getenv("OWNER_ID", None))
 
 # Fill Queue Limit . Example - 15
-QUEUE_LIMIT = int(getenv("QUEUE_LIMIT", "10"))
+QUEUE_LIMIT = int(getenv("QUEUE_LIMIT", "5000"))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -48,8 +48,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/TamilBots")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/TamilSupport")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/NandhaBots")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/NandhaSupport")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
